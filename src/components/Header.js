6272ff6694057835f34ae4b2ce465100da06fa7c
Semalmahajan <64732701+Semalmahajan@users.noreply.github.com>
2024-06-17 15:58:59 +0530

    success page

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,6 +30,15 @@ const Header = () => {
       toast.success(data.message);
       dispatch(setUserDetails(null));
       localStorage.clear();
+      const cookies = document.cookie.split(";");
+
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i];
+        const eqPos = cookie.indexOf("=");
+        const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie =
+          name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
+      }
       navigate("/login");
     }
 
