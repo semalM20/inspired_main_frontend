1d819c80a29e05b9cb70680b59054e5fdf4fb0b9
Semalmahajan <64732701+Semalmahajan@users.noreply.github.com>
2024-06-18 20:41:38 +0530

    review updated

--- src/pages/reviews/ReviewForm.js
+++ src/pages/reviews/ReviewForm.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import axios from "axios";
 import SummaryApi from "../../common";
 import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
+import RedirectToLogin from "../../components/RedirectToLogin";
 
 const formStyle = {
   display: "flex",
@@ -26,6 +28,8 @@ const appStyle = {
 };
 
 function ReviewForm() {
+  const user = useSelector((state) => state?.user?.user);
+
   const [name, setName] = useState("");
   const [rating, setRating] = useState("");
   const [comment, setComment] = useState("");
@@ -45,47 +49,53 @@ function ReviewForm() {
 
   return (
     <>
-      <div
-        style={appStyle}
-        className="flex flex-col justify-center items-center"
-      >
-        <h1 className="uppercase bg-red-700 text-white w-[266px] h-8 text-center mb-2 p-1 rounded font-medium">
-          Reviews
-        </h1>
-      </div>
-      <form style={formStyle} onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          style={inputStyle}
-          required
-        />
-        <input
-          type="number"
-          placeholder="Rating (1-5)"
-          value={rating}
-          onChange={(e) => setRating(e.target.value)}
-          style={inputStyle}
-          min="1"
-          max="5"
-          required
-        />
-        <textarea
-          placeholder="Comment"
-          value={comment}
-          onChange={(e) => setComment(e.target.value)}
-          style={inputStyle}
-          required
-        />
-        <button
-          type="submit"
-          className="w-fit mx-auto block py-1 px-3 rounded bg-red-600 text-white hover:bg-red-700"
-        >
-          Submit
-        </button>
-      </form>
+      {user?._id ? (
+        <>
+          <div
+            style={appStyle}
+            className="flex flex-col justify-center items-center"
+          >
+            <h1 className="uppercase bg-red-700 text-white w-[266px] h-8 text-center mb-2 p-1 rounded font-medium">
+              Reviews
+            </h1>
+          </div>
+          <form style={formStyle} onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              style={inputStyle}
+              required
+            />
+            <input
+              type="number"
+              placeholder="Rating (1-5)"
+              value={rating}
+              onChange={(e) => setRating(e.target.value)}
+              style={inputStyle}
+              min="1"
+              max="5"
+              required
+            />
+            <textarea
+              placeholder="Comment"
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+              style={inputStyle}
+              required
+            />
+            <button
+              type="submit"
+              className="w-fit mx-auto block py-1 px-3 rounded bg-red-600 text-white hover:bg-red-700"
+            >
+              Submit
+            </button>
+          </form>
+        </>
+      ) : (
+        <RedirectToLogin />
+      )}
     </>
   );
 }
